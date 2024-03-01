05c4434d6054e36532be4359b2cd283122ae13ae
Aron Vargas <ajvargas@sbcglobal.net>
2024-02-29 18:02:02 -0800

    Added missing logout route

--- freedom/routes/home.php
+++ freedom/routes/home.php
@@ -9,6 +9,7 @@ $app->get('/register', [\HomeController::class, 'register'])->setName('register'
 $app->post('/register', [\HomeController::class, 'create']);
 $app->get('/login', [\HomeController::class, 'login'])->setName('login');
 $app->post('/login', [\HomeController::class, 'authenticate']);
+$app->get('/logout', [\HomeController::class, 'logout'])->setName('logout');
 $app->get('/forgot-password', [\HomeController::class, 'forgot_password'])->setName('password.forgot');
 $app->post('/forgot-password', [\HomeController::class, 'new_password']);
 $app->get('/reset-password/{token}', [\HomeController::class, 'reset_create'])->setName('password.reset');
